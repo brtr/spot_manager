8d5a8abeadbd827d7b77cb63134c537fa4a7a87e
Pan Xiong <xp0729@gmail.com>
2024-02-28 18:43:26 +0800

    Update campaign filter page

--- app/services/campaign_filter_service.rb
+++ app/services/campaign_filter_service.rb
@@ -12,7 +12,7 @@ class CampaignFilterService
         total_estimated_revenue = origin_txs.select{|r| r.trade_type == 'buy'}.sum(&:revenue)
         total_roi = total_cost.zero? ? 0 : total_estimated_revenue / total_cost
         result[campaign] = {
-          txs: origin_txs,
+          txs: aggregate_txs(origin_txs),
           profit_count: profit_records.count,
           profit_amount: calculate_field(profit_records),
           loss_count: loss_records.count,
@@ -25,6 +25,37 @@ class CampaignFilterService
       end
 
       result
+    end
+
+    def aggregate_txs(txs)
+      data = []
+      percent_changes = CoinService.get_percent_changes(txs.map(&:from_symbol).uniq)
+      txs.select{|x| x.trade_type == 'buy'}.sort_by{|x| x.event_time}.group_by(&:from_symbol).each do |from_symbol, origin_txs|
+        total_cost = 0
+        total_qty = 0
+        total_fee = 0
+
+        origin_txs.each do |tx|
+          total_cost += tx.amount
+          total_qty += tx.qty
+          total_fee += tx.fee
+        end
+
+        origin_tx = origin_txs.last
+        to_symbol = origin_tx.to_symbol
+        price = total_qty.zero? ? 0 : total_cost / total_qty
+        revenue = origin_tx.current_price * total_qty - total_cost
+        roi = revenue / total_cost.abs
+        r = percent_changes.select{|pc| pc[0].upcase == from_symbol}.first || []
+
+        data.push({
+          symbol: "#{from_symbol}#{to_symbol}", cost: total_cost,
+          price: price, qty: total_qty, amount: total_cost,
+          current_price: origin_tx.current_price, revenue: revenue,
+          percentage_24h: r[1], percentage_7d: r[2]
+        })
+      end
+      data
     end
 
     def calculate_field(records, field_name = :revenue)
